72368889eb1a9b5c95f96eec2cfacd2d530aac4a
kobi-kuma <kobi.kuma@kakaocorp.com>
2017-01-03 14:23:46 +0900

    fix for korean subtitles

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@
 echo 'Downloading dataset...'
 
 #download dataset first,
-wget http://opus.lingfil.uu.se/download.php?f=OpenSubtitles/en.tar.gz
+http://opus.lingfil.uu.se/download.php?f=OpenSubtitles2016%2Fko.tar.gz
+#wget http://opus.lingfil.uu.se/download.php?f=OpenSubtitles/en.tar.gz
 
 echo 'Unzipping dataset...'
 
 #Making assumption that user hasn't put any other tar files in folder
 #Two tar ball extractions because during testing it downloaded as .gz once?
-tar -xvf *.tar
-tar -xvf *.gz
+tar -xzvf *.tar.gz
 
 echo 'Extracting dataset...'
 
